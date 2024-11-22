a4c8e2d0a3739e47c559fde76d8ee0a4fa772c97
João Paulo Leite Costa <joaopaulotds@gmail.com>
2024-11-22 16:24:20 -0300

    Conectando a api ao mongodb

---
+++ src/controllers/postsController.js
@@ -0,0 +1,8 @@
+import { getTodosPosts } from "../models/postsModel.js";
+
+
+export async function listarPosts (req, res)
+{
+    const posts =  await getTodosPosts();
+    res.status(200).json(posts);
+}
